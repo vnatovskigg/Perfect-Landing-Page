2db3f2db256c385141606a9ff5300fb43d004a85
vnatovskigg <49907900+vnatovskigg@users.noreply.github.com>
2020-10-27 09:31:22 +0200

    initialize app

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function imgSlider(src) {
+    document.querySelector('.pepsi').src = src;
+}
+
+function changeBG(color) {
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
