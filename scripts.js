34be923951591508c2de35625d6ceffa63074642
vnatovskigg <49907900+vnatovskigg@users.noreply.github.com>
2020-10-27 11:34:22 +0200

    feat: responsive features

--- scripts.js
+++ scripts.js
@@ -6,3 +6,10 @@ function changeBG(color) {
     const sec = document.querySelector('.sec');
     sec.style.background = color;
 }
+
+function menuToggle() {
+    const menu = document.querySelector('.toggle');
+    const nav = document.querySelector('.nav');
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+}
